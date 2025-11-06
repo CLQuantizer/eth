c02ad488a61a32d9de15af39e74778d749fb23b2
gxfc <hello@gongxifacai.win>
2025-11-06 03:12:26 +0100

    go

--- erc20/deploy_ezusd.sol
+++ erc20/deploy_ezusd.sol
@@ -7,9 +7,7 @@ import "./ezusd.sol";
  * @title DeployEZUSD
  * @dev Deployment script for EZUSD with ezusd.png logo
  * 
- * Note: Replace the logoURI with the actual URL/IPFS hash where ezusd.png is hosted
- * Example IPFS: "ipfs://QmYourHashHere/ezusd.png"
- * Example HTTP: "https://yourdomain.com/assets/ezusd.png"
+ * Logo is hosted at: https://ezusd.gongxifacai.win/ezusd.png
  */
 contract DeployEZUSD {
     function deploy() public returns (ezUSD) {
@@ -18,7 +16,7 @@ contract DeployEZUSD {
             "EZUSD",                // symbol
             6,                      // decimals
             1000000000 * 10**6,     // totalSupply: 1 billion tokens
-            "ipfs://QmYourHashHere/ezusd.png"  // logoURI - replace with actual IPFS hash or URL
+            "https://ezusd.gongxifacai.win/ezusd.png"  // logoURI
         );
     }
 }
